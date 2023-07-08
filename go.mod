33250a28a91e12161296eb351b4af0cced449533
pibragimov <pibragimov@whoosh.bike>
2023-07-08 14:30:49 +0300

    feat: attempt to migrate to gotgproto

--- go.mod
+++ go.mod
@@ -16,6 +16,8 @@ require (
 )
 
 require (
+	github.com/AnimeKaizoku/cacher v1.0.1 // indirect
+	github.com/anonyindian/gotgproto v1.0.0-beta10 // indirect
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/go-faster/errors v0.6.1 // indirect
@@ -23,9 +25,13 @@ require (
 	github.com/go-faster/xor v1.0.0 // indirect
 	github.com/gotd/ige v0.2.2 // indirect
 	github.com/gotd/neo v0.1.5 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.16.6 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
 	go.opentelemetry.io/otel v1.16.0 // indirect
@@ -37,6 +43,8 @@ require (
 	golang.org/x/net v0.11.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/sys v0.9.0 // indirect
+	gorm.io/driver/sqlite v1.4.4 // indirect
+	gorm.io/gorm v1.24.6 // indirect
 	nhooyr.io/websocket v1.8.7 // indirect
 	rsc.io/qr v0.2.0 // indirect
 )
--- internal/presentation/telegram/spam_reaction_command.go
+++ internal/presentation/telegram/spam_reaction_command.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/anonyindian/gotgproto/functions"
 	"github.com/gotd/td/bin"
 	"github.com/gotd/td/telegram/message"
 	"github.com/gotd/td/telegram/message/html"
@@ -77,72 +78,82 @@ func (r *Presentation) spamReactionCommandHandler(
 		return err
 	}
 
-	r.telegramApi.MessagesGetMessages()
-	res, err := r.telegramApi.MessagesGetMessages(ctx, []tg.InputMessageClass{&tg.InputMessageID{ID: mReplyHeader.GetReplyToMsgID()}})
+	utils.LogInterface(m)
+	utils.LogInterface(mReplyHeader)
+	chatId := tgUtils.GetChatOrChannelId(entities)
+	//chat, err := functions.GetChatFromPeer(ctx, r.telegramApi, m.ID)
+	//if err != nil {
+	//	return err
+	//}
+
+	res, err := functions.GetMessages(ctx, r.telegramApi, chatId, []tg.InputMessageClass{&tg.InputMessageID{ID: mReplyHeader.GetReplyToMsgID()}})
 	if err != nil {
 		return err
 	}
-	repliedMessages, ok := res.(*tg.MessagesMessages)
+	if len(res) <= 0 {
+		return errors.Join(errors.New("replied message not found"), BadUpdate)
+	}
+	repliedMessages, ok := res[0].(*tg.Message)
 	if err != nil {
 		return errors.Join(errors.New("no replied message found"), BadUpdate)
 	}
 	utils.LogInterface(repliedMessages)
 
-	var victimUserId int64
-	for _, value := range repliedMessages.Users {
-		victimUserId = value.GetID()
-		break
-	}
-	if victimUserId == 0 {
-		victimUserId, err = tgUtils.GetSelfId(ctx, r.telegramClient)
-		if err != nil {
-			return err
-		}
-	}
-	victimChatId := tgUtils.GetChatOrChannelId(entities)
-	if victimChatId == 0 {
-		victimChatId, err = tgUtils.GetSelfId(ctx, r.telegramClient)
-		if err != nil {
-			return err
-		}
-	}
-
-	reactionRequest := tg.MessagesSendReactionRequest{Peer: tgUtils.GetPeer(entities), MsgID: m.GetID(), AddToRecent: true}
-	for _, repliedMessage := range repliedMessages.GetMessages() {
-		messageImpl, ok := repliedMessage.(*tg.Message)
-		utils.LogInterface(messageImpl)
-
-		if !ok {
-			log.Warn().Str("status", "not.a.message").Send()
-			continue
-		}
-		for idx, currentReaction := range messageImpl.Reactions.Results {
-			if idx >= maxReactionCount {
-				break
-			}
-
-			reactionRequest.Reaction = append(reactionRequest.Reaction, currentReaction.Reaction)
-		}
-	}
-	if len(reactionRequest.Reaction) == 0 {
-		_, err = r.telegramSender.Reply(*entities, update).StyledText(ctx, html.String(nil, "Err: no reactions were found"))
-		return err
-	}
-
-	buf := bin.Buffer{}
-	err = reactionRequest.Encode(&buf)
-	if err != nil {
-		return err
-	}
-
-	key := compileSmapKey(victimChatId, victimUserId)
-	err = r.storage.Save(key, buf.Buf)
-	if err != nil {
-		return err
-	}
-
-	log.Info().Str("status", "smap.reactions.saved").Str("key", key).Interface("reactions", reactionRequest).Send()
-	_, err = r.telegramSender.Reply(*entities, update).
-		StyledText(ctx, html.String(nil, "Ok: reactions were saved"))
+	//var victimUserId int64
+	//for _, value := range repliedMessages.Users {
+	//	victimUserId = value.GetID()
+	//	break
+	//}
+	//if victimUserId == 0 {
+	//	victimUserId, err = tgUtils.GetSelfId(ctx, r.telegramClient)
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
+	//victimChatId := tgUtils.GetChatOrChannelId(entities)
+	//if victimChatId == 0 {
+	//	victimChatId, err = tgUtils.GetSelfId(ctx, r.telegramClient)
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
+	//
+	//reactionRequest := tg.MessagesSendReactionRequest{Peer: tgUtils.GetPeer(entities), MsgID: m.GetID(), AddToRecent: true}
+	//for _, repliedMessage := range repliedMessages.GetMessages() {
+	//	messageImpl, ok := repliedMessage.(*tg.Message)
+	//	utils.LogInterface(messageImpl)
+	//
+	//	if !ok {
+	//		log.Warn().Str("status", "not.a.message").Send()
+	//		continue
+	//	}
+	//	for idx, currentReaction := range messageImpl.Reactions.Results {
+	//		if idx >= maxReactionCount {
+	//			break
+	//		}
+	//
+	//		reactionRequest.Reaction = append(reactionRequest.Reaction, currentReaction.Reaction)
+	//	}
+	//}
+	//if len(reactionRequest.Reaction) == 0 {
+	//	_, err = r.telegramSender.Reply(*entities, update).StyledText(ctx, html.String(nil, "Err: no reactions were found"))
+	//	return err
+	//}
+	//
+	//buf := bin.Buffer{}
+	//err = reactionRequest.Encode(&buf)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//key := compileSmapKey(victimChatId, victimUserId)
+	//err = r.storage.Save(key, buf.Buf)
+	//if err != nil {
+	//	return err
+	//}
+	//
+	//log.Info().Str("status", "smap.reactions.saved").Str("key", key).Interface("reactions", reactionRequest).Send()
+	//_, err = r.telegramSender.Reply(*entities, update).
+	//	StyledText(ctx, html.String(nil, "Ok: reactions were saved"))
 	return err
 }
